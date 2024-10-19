065151dbf76bfe5221e49b8a596f87af606f8335
Josh Nussbaum <joshnuss@gmail.com>
2024-10-19 02:32:08 -0400

    More debugging

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,14 @@
 import { Database, OPEN_READONLY } from 'duckdb-async'
+import fs from 'fs/promises'
 
 export async function load() {
-  console.log(process.cwd())
+  console.log('cwd', process.cwd())
+  const files = await fs.readdir(process.cwd())
+
+  files.forEach(file => {
+    console.log(file)
+  })
+
   const db = await Database.create('example.duckdb', OPEN_READONLY)
 
   const products = await db.all('select * from products')
