d0ad59fb1b9b46a74eca227c05f4e01adeb85116
Josh Nussbaum <joshnuss@gmail.com>
2024-10-19 02:36:01 -0400

    Output /usr

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,13 @@
 import { Database, OPEN_READONLY } from 'duckdb-async'
 import fs from 'fs/promises'
+import path from 'path'
 
 export async function load() {
   console.log('cwd', process.cwd())
-  const files = await fs.readdir(process.cwd())
+  const files = await fs.readdir(path.join(process.cwd(), 'usr'))
 
   files.forEach(file => {
-    console.log(file)
+    console.log('/usr', file)
   })
 
   const db = await Database.create('example.duckdb', OPEN_READONLY)
